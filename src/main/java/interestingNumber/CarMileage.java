4a21293f9136f5eac6c5035abd826b8133aa4680
martin.zhou@oocl.com <martin.zhou@oocl.com>
2019-01-21 19:41:28 +0800

    add ninth test and pass test

--- src/main/java/interestingNumber/CarMileage.java
+++ src/main/java/interestingNumber/CarMileage.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 public class CarMileage {
 
-  public static int isInteresting(int number) {
+  public static int isInteresting(int number, Integer [] awesomePhrases) {
     if(number < 100)
       return 0;
     else {
--- src/test/java/interestingNumber/CarMileageTest.java
+++ src/test/java/interestingNumber/CarMileageTest.java
@@ -9,7 +9,7 @@ public class CarMileageTest {
   public void shoule_return_0_when_check_interesting_number_given_invalid_number_0() {
     int inputNumber = 0;
     int expectedResult = 0;
-    int actualResult = CarMileage.isInteresting(0);
+    int actualResult = CarMileage.isInteresting(0, new Integer[]{});
     Assert.assertEquals(expectedResult, actualResult);
   }
 
@@ -17,7 +17,7 @@ public class CarMileageTest {
   public void should_return_0_when_check_interesting_number_given_number_1_not_in_the_range_of_interesting() {
     int inputNumber = 1;
     int expectedResult = 0;
-    int actualResult = CarMileage.isInteresting(inputNumber);
+    int actualResult = CarMileage.isInteresting(inputNumber, new Integer[]{});
     Assert.assertEquals(expectedResult, actualResult);
   }
 
@@ -25,7 +25,7 @@ public class CarMileageTest {
   public void should_return_0_when_check_interesting_number_given_number_digit_followed_by_all_zeros_100() {
     int inputNumber = 100;
     int expectedResult = 2;
-    int actualResult = CarMileage.isInteresting(inputNumber);
+    int actualResult = CarMileage.isInteresting(inputNumber, new Integer[]{});
     Assert.assertEquals(expectedResult, actualResult);
   }
 
@@ -33,7 +33,7 @@ public class CarMileageTest {
   public void should_return_0_when_check_interesting_number_given_number_digit_is_the_same_number_111() {
     int inputNumber = 111;
     int expectedResult = 2;
-    int actualResult = CarMileage.isInteresting(inputNumber);
+    int actualResult = CarMileage.isInteresting(inputNumber, new Integer[]{});
     Assert.assertEquals(expectedResult, actualResult);
   }
 
@@ -41,14 +41,14 @@ public class CarMileageTest {
   public void should_return_0_when_check_interesting_number_given_number_digits_are_sequential_and_increment_123() {
     int inputNumber = 123;
     int expectedResult = 2;
-    int actualResult = CarMileage.isInteresting(inputNumber);
+    int actualResult = CarMileage.isInteresting(inputNumber, new Integer[]{});
     Assert.assertEquals(expectedResult, actualResult);
   }
   @Test
   public void should_return_0_when_check_interesting_number_given_number_digits_are_sequential_and_incement_890() {
     int inputNumber = 890;
     int expectedResult = 2;
-    int actualResult = CarMileage.isInteresting(inputNumber);
+    int actualResult = CarMileage.isInteresting(inputNumber, new Integer[]{});
     Assert.assertEquals(expectedResult, actualResult);
   }
 
@@ -56,16 +56,23 @@ public class CarMileageTest {
   public void should_return_0_when_check_interesting_number_given_number_digits_are_sequential_and_decrement_321() {
     int inputNumber = 321;
     int expectedResult = 2;
-    int actualResult = CarMileage.isInteresting(inputNumber);
+    int actualResult = CarMileage.isInteresting(inputNumber, new Integer[]{});
     Assert.assertEquals(expectedResult, actualResult);
   }
   @Test
   public void should_return_0_when_check_interesting_number_given_number_digits_are_a_palindrome_121() {
     int inputNumber = 121;
     int expectedResult = 2;
-    int actualResult = CarMileage.isInteresting(inputNumber);
+    int actualResult = CarMileage.isInteresting(inputNumber, new Integer[]{});
     Assert.assertEquals(expectedResult, actualResult);
   }
 
+  @Test
+  public void should_return_0_when_check_interesting_number_given_number_digits_124_not_match_awesomePhrases() {
+    int inputNumber = 124;
+    int expectedResult = 0;
+    int actualResult = CarMileage.isInteresting(inputNumber, new Integer[]{});
+    Assert.assertEquals(expectedResult, actualResult);
+  }
 
 }
