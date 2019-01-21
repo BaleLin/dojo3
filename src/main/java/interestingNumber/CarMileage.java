a9605338a2d4548745fd52110b5824bc6d9d2874
martin.zhou@oocl.com <martin.zhou@oocl.com>
2019-01-21 18:59:04 +0800

    add code for first test.

--- src/main/java/interestingNumber/CarMileage.java
+++ src/main/java/interestingNumber/CarMileage.java
@@ -3,6 +3,8 @@ package interestingNumber;
 public class CarMileage {
 
   public static int isInteresting(int number) {
-    return 0;
+    if(number < 1)
+      return 0;
+    else return 2;
   }
 }
