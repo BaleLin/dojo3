a583aac5d01c157e4ed7fe05af1ca1dd3890280f
martin.zhou@oocl.com <martin.zhou@oocl.com>
2019-01-21 19:12:43 +0800

    add code for third test

--- src/main/java/interestingNumber/CarMileage.java
+++ src/main/java/interestingNumber/CarMileage.java
@@ -1,10 +1,36 @@
 package interestingNumber;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarMileage {
 
   public static int isInteresting(int number) {
     if(number < 100)
       return 0;
-    else return 2;
+    else {
+      if (digitFollowedByZeros(number))
+        return 2;
+      else return 0;
+    }
+  }
+
+  private static boolean digitFollowedByZeros(int number) {
+    Integer[] array = numberToDigitArray(number);
+    int multiple = array.length - 1;
+    return number % Math.pow(10, multiple) == 0;
+  }
+
+  private static Integer[] numberToDigitArray(int number) {
+    List<Integer> digitList = new ArrayList<Integer>();
+    int digit = number % 10;
+    digitList.add(digit);
+    number /= 10;
+    while(number > 0) {
+      digit = number % 10;
+      digitList.add(digit);
+      number /= 10;
+    }
+    return digitList.toArray(new Integer[0]);
   }
 }
